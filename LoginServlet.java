3ae207f8b2873685082034070d62395fea47815e
thomas-bios <32367331+thomas-bios@users.noreply.github.com>
2019-05-08 22:46:27 -0700

    Add files via upload

--- LoginServlet.java
+++ LoginServlet.java
@@ -1,12 +1,16 @@
 package jeNuage;
 
 import java.io.IOException;
+import java.util.Base64;
+
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 
 @WebServlet("/JeNuage/LoginServlet")
 public class LoginServlet extends HttpServlet {
@@ -25,5 +29,31 @@ public class LoginServlet extends HttpServlet {
 		response.getWriter().append("ok " + name + " : " + pass);
 
 	}
+	
+	
+	//The following is not from us; from https://stackoverflow.com/questions/2860943/how-can-i-hash-a-password-in-java
+	
+	private static final int iterations = 2000;
+    private static final int desiredKeyLen = 128;
+	public static boolean check(String password, String stored) throws Exception{
+    String[] saltAndHash = stored.split("\\$");
+    if (saltAndHash.length != 2) {
+        throw new IllegalStateException(
+            "The stored password must have the form 'salt$hash'");
+    }
+    String hashOfInput = hash(password, Base64.getDecoder().decode(saltAndHash[0]));
+    return hashOfInput.equals(saltAndHash[1]);
+    
+    
+	}
+	private static String hash(String password, byte[] salt) throws Exception {
+        if (password == null || password.length() == 0)
+            throw new IllegalArgumentException("Empty passwords are not supported.");
+        SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        SecretKey key = f.generateSecret(new PBEKeySpec(
+            password.toCharArray(), salt, iterations, desiredKeyLen));
+        return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
 
 }
+
--- SignupServlet.java
+++ SignupServlet.java
@@ -1,6 +1,12 @@
 package jeNuage;
 
 import java.io.IOException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -20,4 +26,24 @@ public class SignupServlet extends HttpServlet {
 		doGet(request, response);
 	}
 
+	
+	//The following is not from us; from https://stackoverflow.com/questions/2860943/how-can-i-hash-a-password-in-java
+
+	
+	private static final int iterations = 2000;
+    private static final int saltLen = 16;
+    private static final int desiredKeyLen = 128;
+
+    public static String getSaltedHash(String password) throws Exception {
+        byte[] salt = SecureRandom.getInstance("SHA1PRNG").generateSeed(saltLen);
+        return Base64.getEncoder().encodeToString(salt) + "$" + hash(password, salt);
+    }
+    private static String hash(String password, byte[] salt) throws Exception {
+        if (password == null || password.length() == 0)
+            throw new IllegalArgumentException("Empty passwords are not supported.");
+        SecretKeyFactory f = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        SecretKey key = f.generateSecret(new PBEKeySpec(
+            password.toCharArray(), salt, iterations, desiredKeyLen));
+        return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
 }
