2ae7c6552ec712a4ffd49206deabb24cac93231a
thomas-bios <32367331+thomas-bios@users.noreply.github.com>
2019-05-13 19:48:51 -0700

    download

--- Action.java
+++ Action.java
@@ -1,9 +1,12 @@
 package iNuage;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,22 +14,27 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import org.apache.commons.io.FileUtils;
 
 import iNuage.Sql_id;
 
-@WebServlet(name = "Actions", urlPatterns = { "/iNuage/action" })
+@WebServlet(name = "Action", urlPatterns = { "/iNuage/action" })
 public class Action extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		String action = request.getParameter("c");
-		if(session.getAttribute("user_id_int") == null) {
-			request.getRequestDispatcher("iNuage/home.jsp").forward(request, response);
-			return;
+		
+		if(action.equals("down")) {
+			downloadFile((int)session.getAttribute("user_id_int"),request.getParameter("fid"));
+			response.sendRedirect(request.getContextPath() + "/iNuage");
 		}
 		
-		
+		if(session.getAttribute("user_id_int") == null) {
+			response.sendRedirect(request.getContextPath() + "/iNuage");
+			return;
+		}
 		switch(action) {
 			case("logout"):
 				logout(request);
@@ -35,9 +43,6 @@ public class Action extends HttpServlet {
 			case("delete"):
 				deleteAccount((String)session.getAttribute("user_id_string"),request);
 				response.sendRedirect(request.getContextPath() + "/iNuage?status=5");
-				break;
-			case("down"):
-				downloadFile((String)session.getAttribute("user_id_string"),request.getParameter("fid"));
 				break;
 			case("del"):
 				removeFile((String)session.getAttribute("user_id_string"),Integer.parseInt(request.getParameter("fid")));
@@ -64,6 +69,27 @@ public class Action extends HttpServlet {
 		return true;
 	}
 	
+	protected boolean downloadFile(int user_id, String file_id) {
+		try {
+			Connection con = DriverManager.getConnection(Sql_id.connection, Sql_id.user, Sql_id.password);
+			String l = "select * from jenuage_docs where file_id = \"" + file_id + "\";";
+			PreparedStatement pst = con.prepareStatement(l);
+			ResultSet result = pst.executeQuery();
+			
+			if(result.next()) {
+				if(result.getInt(5) == 1 || result.getInt(1)  == user_id) {
+					FileUtils.copyURLToFile(new URL(result.getString(3)), 
+							  new File(result.getString(4)), 10000, 10000);
+				}
+			}
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+	
 	protected boolean deleteAccount(String user_id,HttpServletRequest request) {
 		try {		
 			Connection con = DriverManager.getConnection(Sql_id.connection, Sql_id.user, Sql_id.password);
@@ -84,46 +110,30 @@ public class Action extends HttpServlet {
 		return true;
 	}
 	
-	protected boolean downloadFile(String user_id, String file_name) {
-		
-		
-		return true;
-	}
-	
-	protected void removeFile(String user_id, int file_id )
-	{
-		try
-		{		
+	protected void removeFile(String user_id, int file_id )	{
+		try	{		
 			Connection con = DriverManager.getConnection(Sql_id.connection, Sql_id.user, Sql_id.password);
 			
 			String l = "delete from jenuage_docs where user = \"" + user_id + "\"and file_id = " + file_id + ";";
 			PreparedStatement pst = con.prepareStatement(l);
 			pst.executeUpdate();
 			
-		} catch(Exception e) {
-
-		}
+		} catch(Exception e) {}
 	}
 	
-	protected void renameFile(String user_id, int file_id, String newname)
-	{
-		try
-		{		
+	protected void renameFile(String user_id, int file_id, String newname){
+		try	{		
 			Connection con = DriverManager.getConnection(Sql_id.connection, Sql_id.user, Sql_id.password);
 			
 			String l = "update jenuage_docs set name=\"" + newname + "\" where user = \"" + user_id + "\"and file_id = " + file_id + ";";
 			PreparedStatement pst = con.prepareStatement(l);
 			pst.executeUpdate();
 			
-		} catch(Exception e) {
-
-		}
+		} catch(Exception e) {}
 	}
 	
-	protected void shareFile(String user_id, int file_id, int state)
-	{
-		try
-		{		
+	protected void shareFile(String user_id, int file_id, int state){
+		try	{		
 			Connection con = DriverManager.getConnection(Sql_id.connection, Sql_id.user, Sql_id.password);
 			String l;
 			if(state == 1)
@@ -133,16 +143,6 @@ public class Action extends HttpServlet {
 			PreparedStatement pst = con.prepareStatement(l);
 			pst.executeUpdate();
 			
-		} catch(Exception e) {
-
-		}
+		} catch(Exception e) {}
 	}
-	
 }
-
-
-
-
-
-
-
