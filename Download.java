9db29fc25d386585dba8a9ebac880c4d9e7017a5
thomas-bios <32367331+thomas-bios@users.noreply.github.com>
2019-05-13 22:30:51 -0700

    Downloading file
    only work in server

--- Download.java
+++ Download.java
@@ -14,6 +14,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileSystemView;
 
 import org.apache.commons.io.FileUtils;
 
@@ -25,7 +27,7 @@ public class Download extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		String file_id = request.getParameter("fid");
-		int user_id = null == session.getAttribute("user_id_int")? (int) session.getAttribute("user_id_int"): -1 ;
+		int user_id = null != session.getAttribute("user_id_int")? (int) session.getAttribute("user_id_int"): -1 ;
 		try {
 			Connection con = DriverManager.getConnection(Sql_id.connection, Sql_id.user, Sql_id.password);
 			String l = "select * from jenuage_docs where file_id = \"" + file_id + "\";";
@@ -34,10 +36,38 @@ public class Download extends HttpServlet {
 			
 			if(result.next()) {
 				if(result.getInt(5) == 1 || (user_id != -1 && result.getInt(1)  == user_id)) {
-					FileUtils.copyURLToFile(new URL(result.getString(3)), 
-							  new File(result.getString(4)), 10000, 10000);
+					
+					//DONT WORK IN LOCAL : ONLY WITH HTTP://FILE
+					
+					//if in serv
+					//String url = result.getString(3).replace("\\","/");
+					//if in local : stupid file for testing
+					String url = "https://www.j2eebrain.com/wp-content/uploads/c11e504954111c54b467c154d4abc2d3.png";
+					String newpath = "/home/" + result.getString(4);
+
+					//dialog box for choosing the new path
+					JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+					jfc.setDialogTitle("Choose a directory to save your file: ");
+					jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+					int returnValue = jfc.showSaveDialog(null);
+					if (returnValue == JFileChooser.APPROVE_OPTION && jfc.getSelectedFile().isDirectory()) {
+						newpath = jfc.getSelectedFile().getAbsolutePath();
+					} else {
+						//error choosing the path
+						throw new Exception();
+					}
+					
+					newpath += "/" + result.getString(4);
+					
+					//downloading 
+					FileUtils.copyURLToFile(new URL(url), 
+							  new File(newpath), 10000, 10000);
+					
+					response.sendRedirect(request.getContextPath() + "/iNuage?path="+newpath);
+					return;
 				}else {
-					//request forward some jsp wrong rights
+					request.getRequestDispatcher("unauthorized.jsp").forward(request, response);
 					return;
 				}
 			}
