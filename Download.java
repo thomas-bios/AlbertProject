f4b644ea9d888712680868ec86c6003e4adb0d0b
thomas-bios <32367331+thomas-bios@users.noreply.github.com>
2019-05-17 17:12:54 -0700

    download

--- Download.java
+++ Download.java
@@ -1,23 +1,22 @@
 package iNuage;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.swing.JFileChooser;
-import javax.swing.filechooser.FileSystemView;
 
-import org.apache.commons.io.FileUtils;
 
 
 @WebServlet("/iNuage/Download")
@@ -28,54 +27,59 @@ public class Download extends HttpServlet {
 		HttpSession session = request.getSession();
 		String file_id = request.getParameter("fid");
 		int user_id = null != session.getAttribute("user_id_int")? (int) session.getAttribute("user_id_int"): -1 ;
+		
 		try {
 			Connection con = DriverManager.getConnection(Sql_id.connection, Sql_id.user, Sql_id.password);
 			String l = "select * from jenuage_docs where file_id = \"" + file_id + "\";";
 			PreparedStatement pst = con.prepareStatement(l);
 			ResultSet result = pst.executeQuery();
 			
+			
 			if(result.next()) {
-				if(result.getInt(5) == 1 || (user_id != -1 && result.getInt(1)  == user_id)) {
-					
-					//DONT WORK IN LOCAL : ONLY WITH HTTP://FILE
-					
+				if(result.getInt("share") == 1 || (user_id != -1 && result.getInt("user")  == user_id)) {
+										
 					//if in serv
-					//String url = result.getString(3).replace("\\","/");
-					//if in local : stupid file for testing
-					String url = "https://www.j2eebrain.com/wp-content/uploads/c11e504954111c54b467c154d4abc2d3.png";
-					String newpath = "/home/" + result.getString(4);
+					String url = result.getString("path").replace("\\","/");
+					String fileServName = url.substring(url.lastIndexOf("/") + 1).trim();
+					String dataDirectory = request.getServletContext().getRealPath("/WEB-INF/uploads/");
+			        Path file = Paths.get(dataDirectory, fileServName);
+		            
+			        String resultName;
+			        if(result.getString("name").indexOf(".") != -1)
+			        	resultName = result.getString("name").substring(0,result.getString("name").indexOf(".")+1) + result.getString("ext");
+			        else
+			        	resultName = result.getString("name") + "." + result.getString("ext");
 
-					//dialog box for choosing the new path
-					JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-					jfc.setDialogTitle("Choose a directory to save your file: ");
-					jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		         // obtains ServletContext
+		            ServletContext context = getServletContext();
+		             
+		            // gets MIME type of the file
+		            String mimeType = context.getMimeType(resultName);
+		            response.setContentType(mimeType);
 
-					int returnValue = jfc.showSaveDialog(null);
-					if (returnValue == JFileChooser.APPROVE_OPTION && jfc.getSelectedFile().isDirectory()) {
-						newpath = jfc.getSelectedFile().getAbsolutePath();
-					} else {
-						//error choosing the path
-						throw new Exception();
-					}
-					
-					newpath += "/" + result.getString(4);
-					
-					//downloading 
-					FileUtils.copyURLToFile(new URL(url), 
-							  new File(newpath), 10000, 10000);
-					
-					response.sendRedirect(request.getContextPath() + "/iNuage?path="+newpath);
+		         // force download
+		            response.setHeader("Content-Disposition", "attachment; filename=" + resultName);
+		            // obtains response's output stream
+		            
+		            Files.copy(file, response.getOutputStream());
+	                response.getOutputStream().flush();
+		             
+
+		            
+		    		//response.getWriter().append(TEST);
+
+					//response.sendRedirect(request.getContextPath() + "/iNuage?path=");
 					return;
-				}else {
+				} else {
 					request.getRequestDispatcher("unauthorized.jsp").forward(request, response);
 					return;
 				}
 			}
 		
 		}catch(Exception e) {
-			e.printStackTrace();
+			response.sendRedirect(request.getContextPath() + "/iNuage?error=" + e.getMessage());
 		}
-		response.sendRedirect(request.getContextPath() + "/iNuage");
+		//response.sendRedirect(request.getContextPath() + "/iNuage");
 
 	}
 
